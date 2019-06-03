a2fa852d032214c66a8041f14b70745f67eb0aed
tyjianchi <tyjianchi@qq.com>
2019-06-03 22:08:59 +0800

    LineRegress_titanic_datafile

--- Data Analysis/SimpleLineRegre.py
+++ Data Analysis/SimpleLineRegre.py
@@ -7,11 +7,12 @@ class dataanalysis:
         self.w=None
 
     def fit(self,X,y):
-        print(X.shape)
+        #print(X.shape)
         X=np.insert(X,0,1,axis=1)
         print(X.shape)
         X_=np.linalg.inv(X.T.dot(X))
         self.w=X_.dot(X.T).dot(y)
+        print(self.w)
 
     def predict(self,X):
         X=np.insert(X,0,1,axis=1)
@@ -26,8 +27,6 @@ class dataanalysis:
         dts=datasets.load_diabetes()
 
         X=dts.data[:,np.newaxis,2]
-        print(dts.data)
-        print(dts.target)
         x_train,x_test=X[:-20],X[-20:]
 
         y_train,y_test=dts.target[:-20],dts.target[-20:]
--- Data Analysis/taitanic_analysis.py
+++ Data Analysis/taitanic_analysis.py
@@ -3,21 +3,49 @@ import numpy as np
 
 
 class titanic_ana:
+    def __init__(self):
+        self.raito=None
+
     def get_data(self,file_path):
         file_data=pd.read_csv(file_path)
-        flag_age=~pd.isnull(file_data["Age"])
-        file_age_ture=file_data[flag_age]
-        pre_x=file_age_ture[["Pclass","Sex","Age","SibSp","Parch"]]
-        post_y=file_age_ture["Fare"]
+        #file_data.dropna(subset=["Age"],axis=0,inplace=True)
+        #file_data.replace({'Sex':{'male':0,'female':1}},inplace=True)
+        pre_x=file_data[["Pclass","SibSp","Parch"]]
+        post_y=file_data["Fare"]
         return pre_x,post_y
 
-    def fit(self,pre_x,post_y):
-        pre_x_=np.linalg.inv(pre_x.T.dot(pre_x))
-        ratio=pre_x_.dot(pre_x.T).dot(post_y)
-        return ratio
+    def test_data(self,file_path):
+        file_data = pd.read_csv(file_path)
+        #file_data.dropna(subset=["Age"],axis=0,inplace=True)
+        #file_data.replace({'Sex':{'male':0,'female':1}},inplace=True)
+        test_x=file_data[["Pclass","SibSp","Parch"]]
+        return test_x
 
+    def fit(self,pre_x,post_y):
+        pre_x.insert(0,"Seq",1)
+        pre_x_=np.linalg.inv(pre_x.T.dot(pre_x))
+        self.raito=pre_x_.dot(pre_x.T).dot(post_y)
+        return self.raito
+
+    def predict(self,test_x):
+        test_x.insert(0, "Seq", 1)
+        return test_x.dot(self.raito)
+
+
+    def test1(self):
+        df = pd.DataFrame(np.random.randn(5, 3), index=list('abcde'),
+                          columns=['one', 'two', 'three'])  # 随机产生5行3列的数据
+        df.iloc[1, :2] = np.nan  # 将指定数据定义为缺失
+        df.iloc[2:-1, 2] = np.nan
+        print(df)
+        df.dropna(subset=["three"],axis=0,inplace=True)
+        print(df)
 if __name__=='__main__':
     ta=titanic_ana()
-    re_x,post_y=ta.get_data(r'/Users/Elven/PycharmProjects/untitled/Data Analysis/train.csv')
-    ratio=ta.fit(re_x,post_y)
-    print(ratio)
+    #ta.test1()
+    pre_x,post_y=ta.get_data(r'/Users/Elven/PycharmProjects/untitled/Data Analysis/train.csv')
+    ta.fit(pre_x,post_y)
+    test_x=ta.test_data(r'/Users/Elven/PycharmProjects/untitled/Data Analysis/test.csv')
+    test_y=ta.predict(test_x)
+    test_y.to_csv(r'/Users/Elven/PycharmProjects/untitled/Data Analysis/result.csv')
+    print(test_y)
